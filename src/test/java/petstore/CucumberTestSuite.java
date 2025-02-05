df4fef733528399265a551b2a2c35403a7c0a32d
anvorguesa <srliposa@gmail.com>
2025-02-04 23:00:18 -0500

    Mantener cambios

---
+++ src/test/java/petstore/CucumberTestSuite.java
@@ -0,0 +1,14 @@
+package petstore;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "classpath:features",
+        tags = "@store"
+)
+public class CucumberTestSuite {
+}
